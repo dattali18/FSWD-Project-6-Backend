7a3a375bd13742243f376de555740d8a60956068
Daniel Attali <danielattali16@gmail.com>
2024-08-18 14:52:40 +0300

    added some changes

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const isWriter = require('../middleware/writerMiddleware');
+const isWriter = require("../middleware/writerMiddleware");
 
-const Article = require('../models/Articles');
+const Article = require("../models/Articles");
 
 /**
  * @desc get an article by id
@@ -11,21 +11,21 @@ const Article = require('../models/Articles');
  * @parm {number} id - article id
  * @access Public
  */
-router.get('/:id', async (req, res) => {
-    let id = req.params.id;
-    // can't access this path why??
-    id = parseInt(id);
-    if (isNaN(id)) {
-        return res.status(400).json({message: "Invalid id"});
-    }
+router.get("/:id", async (req, res) => {
+  let id = req.params.id;
+  // can't access this path why??
+  id = parseInt(id);
+  if (isNaN(id)) {
+    return res.status(400).json({ message: "Invalid id" });
+  }
 
-    try {
-        const article = await Article.getArticlesById(id);
-        return res.status(200).json({article: article});
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({message: "Article not found"});
-    }
+  try {
+    const article = await Article.getArticlesById(id);
+    return res.status(200).json({ article: article });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Article not found" });
+  }
 });
 
 /**
@@ -35,46 +35,46 @@ router.get('/:id', async (req, res) => {
  * @param {number} limit - limit the number of articles
  * @param {number} page - page number
  */
-router.get('/', async (req, res) => {
-    // if no limit or page is provided return all articles
-    let limit = req.query.limit;
-    let page = req.query.page;
+router.get("/", async (req, res) => {
+  // if no limit or page is provided return all articles
+  let limit = req.query.limit;
+  let page = req.query.page;
 
-
-    if (!limit || !page) {
-        try {
-            const articles = await Article.getArticles();
-            return res.status(200).json({articles: articles});
-        } catch (error) {
-            console.log(error);
-            return res.status(400).json({message: error});
-        }
-    }
-
+  if (!limit || !page) {
     try {
-        // try and parse the limit and page
-        limit = parseInt(limit);
-        page = parseInt(page);
-    } catch (e) {
-        return res.status(400).json({message: "Invalid limit or page"});
-    }
-
-    try {
-        let articles = await Article.getArticles();
-        // apply pagination
-        articles = articles.slice((page - 1) * limit, page * limit);
-        // add field for current page and total pages
-        let body = {
-            articles: articles, page: page, totalPages: Math.ceil(articles.length / limit)
-        }
-
-        return res.status(200).json(body);
+      const articles = await Article.getArticles();
+      return res.status(200).json({ articles: articles });
     } catch (error) {
-        console.log(error);
-        return res.status(400).json({message: error});
+      console.log(error);
+      return res.status(400).json({ message: error });
     }
-});
+  }
 
+  try {
+    // try and parse the limit and page
+    limit = parseInt(limit);
+    page = parseInt(page);
+  } catch (e) {
+    return res.status(400).json({ message: "Invalid limit or page" });
+  }
+
+  try {
+    let articles = await Article.getArticles();
+    // apply pagination
+    articles = articles.slice((page - 1) * limit, page * limit);
+    // add field for current page and total pages
+    let body = {
+      articles: articles,
+      page: page,
+      totalPages: Math.ceil(articles.length / limit),
+    };
+
+    return res.status(200).json(body);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: error });
+  }
+});
 
 /**
  * @desc create an article
@@ -85,17 +85,20 @@ router.get('/', async (req, res) => {
  * @param {number} author - article author (user id)
  * @param {[string]} tags - article tags
  */
-router.post('/', isWriter, async (req, res) => {
-    const article = {
-        title: req.body.title, content: req.body.content, author: req.user.id, tags: req.body.tags
-    };
+router.post("/", isWriter, async (req, res) => {
+  const article = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.user.id,
+    tags: req.body.tags,
+  };
 
-    try {
-        const savedArticle = await Article.createArticle(article);
-        return res.status(201).json({article: savedArticle});
-    } catch (error) {
-        return res.status(400).json({message: error});
-    }
+  try {
+    const savedArticle = await Article.createArticle(article);
+    return res.status(201).json({ article: savedArticle });
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
 });
 
 /**
@@ -107,31 +110,33 @@ router.post('/', isWriter, async (req, res) => {
  * @param {string} content - article body
  * @param {[string]} tags - article tags
  */
-router.put('/:id', isWriter, async (req, res) => {
-    let id = req.params.id;
-    id = parseInt(id);
+router.put("/:id", isWriter, async (req, res) => {
+  let id = req.params.id;
+  id = parseInt(id);
 
-    if (isNaN(id)) {
-        return res.status(400).json({message: "Invalid id"});
-    }
+  if (isNaN(id)) {
+    return res.status(400).json({ message: "Invalid id" });
+  }
 
-    // check if the user is the author of the article
-    const article = await Article.getArticlesById(id);
-    if (article.author !== req.user.id) {
-        return res.status(401).json({message: "Unauthorized"});
-    }
+  // check if the user is the author of the article
+  const article = await Article.getArticlesById(id);
+  if (article.author !== req.user.id) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    const articleObject = {
-        title: req.body.title, content: req.body.content, tags: req.body.tags
-    };
+  const articleObject = {
+    title: req.body.title,
+    content: req.body.content,
+    tags: req.body.tags,
+  };
 
-    try {
-        const updatedArticle = await Article.updateArticle(id, articleObject);
-        return res.status(200).json({article: updatedArticle});
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({message: "Article not found"});
-    }
+  try {
+    const updatedArticle = await Article.updateArticle(id, articleObject);
+    return res.status(200).json({ article: updatedArticle });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Article not found" });
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const isAdmin = require('../middleware/adminMiddleware');
-const auth = require('../middleware/authMiddleware');
-const userModel = require('../models/User');
+const isAdmin = require("../middleware/adminMiddleware");
+const auth = require("../middleware/authMiddleware");
+const userModel = require("../models/User");
 
 /**
  * @desc Delete a user by id
  * @route DELETE /users/:id
  * @access Private
  */
-router.delete('/:id', isAdmin, async (req, res) => {
-    const id = req.params.id;
+router.delete("/:id", isAdmin, async (req, res) => {
+  const id = req.params.id;
 
-    try {
-        const [user] = await userModel.getUserById(id);
+  try {
+    const [user] = await userModel.getUserById(id);
 
-        if (!user) {
-            return res.status(404).send('User not found');
-        }
-
-        // delete the user
-        const result = await userModel.deleteUser(id);
-
-        if (result.affectedRows === 1) {
-            return res.status(200).send('User deleted successfully');
-        }
-
-        return res.status(500).send('Internal Server Error');
-    } catch (error) {
-        console.error('Error in deleting user', error);
-        return res.status(500).send('Internal Server Error');
+    if (!user) {
+      return res.status(404).send("User not found");
     }
+
+    // delete the user
+    const result = await userModel.deleteUser(id);
+
+    if (result.affectedRows === 1) {
+      return res.status(200).send("User deleted successfully");
+    }
+
+    return res.status(500).send("Internal Server Error");
+  } catch (error) {
+    console.error("Error in deleting user", error);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 /**
@@ -42,38 +42,38 @@ router.delete('/:id', isAdmin, async (req, res) => {
  * @body {string} user.username
  * @body {string} user.email
  */
-router.put('/:id', auth, async (req, res) => {
-    const id = req.params.id;
-    /**
-     * @type {Object}
-     * @property {string} username
-     * @property {string} email
-     */
-    const userReq = req.body;
+router.put("/:id", auth, async (req, res) => {
+  const id = req.params.id;
+  /**
+   * @type {Object}
+   * @property {string} username
+   * @property {string} email
+   */
+  const userReq = req.body;
 
-    // checking if the user that want to change is the same as the user that is logged in
-    if (req.user.id !== id) {
-        return res.status(403).send('Forbidden access');
+  // checking if the user that want to change is the same as the user that is logged in
+  if (req.user.id !== id) {
+    return res.status(403).send("Forbidden access");
+  }
+
+  try {
+    const [existingUser] = await userModel.getUserById(id);
+
+    if (!existingUser) {
+      return res.status(404).send("User not found");
     }
 
-    try {
-        const [existingUser] = await userModel.getUserById(id);
+    const result = await userModel.updateUser(id, userReq);
 
-        if (!existingUser) {
-            return res.status(404).send('User not found');
-        }
-
-        const result = await userModel.updateUser(id, userReq);
-
-        if (result.affectedRows === 1) {
-            return res.status(200).send('User updated successfully');
-        }
-
-        return res.status(500).send('Internal Server Error');
-    } catch (error) {
-        console.error('Error in updating user', error);
-        return res.status(500).send('Internal Server Error');
+    if (result.affectedRows === 1) {
+      return res.status(200).send("User updated successfully");
     }
+
+    return res.status(500).send("Internal Server Error");
+  } catch (error) {
+    console.error("Error in updating user", error);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 /**
@@ -81,26 +81,26 @@ router.put('/:id', auth, async (req, res) => {
  * @route GET /users/:id
  * @access Private
  */
-router.get('/:id', auth, async (req, res) => {
-    let id = req.params.id;
+router.get("/:id", auth, async (req, res) => {
+  let id = req.params.id;
 
-    // checking if the user that want to change is the same as the user that is logged in
-    if (String(req.user.id) !== id) {
-        return res.status(403).send('Forbidden access');
-    }
+  // checking if the user that want to change is the same as the user that is logged in
+  if (String(req.user.id) !== id) {
+    return res.status(403).send("Forbidden access");
+  }
 
-    try {
-        // return the user without the password
-        const user = {
-            id: req.user.id,
-            username: req.user.username,
-            email: req.user.email
-        };
-        return res.status(200).send(user);
-    } catch (error) {
-        console.error('Error in getting user', error);
-        return res.status(500).send('Internal Server Error');
-    }
+  try {
+    // return the user without the password
+    const user = {
+      id: req.user.id,
+      username: req.user.username,
+      email: req.user.email,
+    };
+    return res.status(200).send(user);
+  } catch (error) {
+    console.error("Error in getting user", error);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 /**
@@ -108,27 +108,27 @@ router.get('/:id', auth, async (req, res) => {
  * @route GET /users/username/:username
  * @access Private
  */
-router.get('/username/:username', auth, async (req, res) => {
-    let username = req.params.username;
+router.get("/username/:username", auth, async (req, res) => {
+  let username = req.params.username;
 
-    // checking if the user that want to change is the same as the user that is logged in
-    if (req.user.username !== username) {
-        return res.status(403).send('Forbidden access');
-    }
+  // checking if the user that want to change is the same as the user that is logged in
+  // if (req.user.username !== username) {
+  //     return res.status(403).send('Forbidden access');
+  // }
 
-    try {
-        // return the user without the password
-        const user = {
-            id: req.user.id,
-            username: req.user.username,
-            email: req.user.email
-        };
-        return res.status(200).send(user);
-    } catch (error) {
-        console.error('Error in getting user', error);
-        return res.status(500).send('Internal Server Error');
-    }
-}
-);
+  try {
+    let [user] = await userModel.getUserByUsername(username);
+    // return the user without the password
+    user = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+    return res.status(200).send(user);
+  } catch (error) {
+    console.error("Error in getting user", error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = router;
